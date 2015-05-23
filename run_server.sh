1e9e5a5f74aac21024ea883812b423408661e4dd
lhmouse <lh_mouse@126.com>
2015-05-23 16:28:14 +0800

    Minor.

--- run_server.sh
+++ run_server.sh
@@ -4,9 +4,9 @@ runpath=`pwd`'/lib/.libs'
 etc=`pwd`'/etc'
 
 if [ "$1" == "-d" ]; then
-	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute gdb --args poseidon $etc/poseidon
+	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute gdb --args poseidon $etc/poseidon-medusa
 elif [ "$1" == "-v" ]; then
-	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' poseidon $etc/poseidon
+	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' poseidon $etc/poseidon-medusa
 else
-	LD_LIBRARY_PATH=$runpath poseidon $etc/poseidon
+	LD_LIBRARY_PATH=$runpath poseidon $etc/poseidon-medusa
 fi
