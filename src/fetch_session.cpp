0a808518551b3fc51d1d14c281927adb8807608c
lhmouse <lh_mouse@126.com>
2015-05-26 11:09:29 +0800

    Minor.

--- src/fetch_session.cpp
+++ src/fetch_session.cpp
@@ -355,7 +355,7 @@ public:
 		PROFILE_ME;
 
 		if(m_connectQueue.empty()){
-			LOG_MEDUSA_ERROR("No connection in progress? fetchUuid = ", m_fetchUuid);
+			LOG_MEDUSA_DEBUG("No connection in progress or connection lost: fetchUuid = ", m_fetchUuid);
 			return false;
 		}
 
