37856600d13c9566b040d2512a2cea1d14564577
lhmouse <lh_mouse@126.com>
2017-04-30 17:23:09 +0800

    .

--- src/encryption.cpp
+++ src/encryption.cpp
@@ -28,21 +28,19 @@ void encrypt(Poseidon::StreamBuffer &dst, const Poseidon::Uuid &uuid, Poseidon::
 	std::memcpy(block_src.data(), sha256.data() + 16, 16);
 	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
 	dst.put(block_dst.data(), 16); // 16 bytes: checksum
-	for(;;){
-		const AUTO(block_plain_len, src.get(block_src.data(), 16));
-		if(block_plain_len < 16){
-			// Append PKCS#7 padding.
-			const unsigned bytes_padded = 16 - block_plain_len;
-			for(unsigned i = 0; i < bytes_padded; ++i){
-				block_src[block_plain_len + i] = bytes_padded;
-			}
-			::AES_cbc_encrypt(block_src.data(), block_dst.data(), block_plain_len, aes_key, iv.data(), AES_ENCRYPT);
-			dst.put(block_dst.data(), 16); // *: final block encrypted
-			break;
-		}
+	// Encrypt payload.
+	const std::size_t n_blocks_m1 = (src.size() + 1) / 16;
+	for(std::size_t i = 0; i < n_blocks_m1; ++i){
+		src.get(block_src.data(), 16);
 		::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
-		dst.put(block_dst.data(), 16); // *: block encrypted
+		dst.put(block_dst.data(), 16);
 	}
+	// Append PKCS#7 padding.
+	const unsigned bytes_padded = 16 - src.get(block_src.data(), 16);
+	LOG_MEDUSA_DEBUG("Appending ", bytes_padded, " padding byte(s).");
+	std::memset(block_src.data() + bytes_padded, static_cast<int>(bytes_padded), bytes_padded);
+	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
+	dst.put(block_dst.data(), 16);
 }
 bool decrypt(Poseidon::Uuid &uuid, Poseidon::StreamBuffer &dst, Poseidon::StreamBuffer src, const std::string &key){
 	PROFILE_ME;
@@ -79,26 +77,27 @@ bool decrypt(Poseidon::Uuid &uuid, Poseidon::StreamBuffer &dst, Poseidon::Stream
 		LOG_MEDUSA_WARNING("Encrypted data is invalid, erroneous checksum.");
 		return false;
 	}
-	if((src.size() - 1) / 16 != src.size() / 16 - 1){ // (size % 16 != 0) || (size == 0)
+	// Decrypt payload.
+	if(src.size() % 16 != 0){
 		LOG_MEDUSA_WARNING("Encrypted data is invalid, invalid size.");
 		return false;
 	}
-	for(;;){
-		const AUTO(block_encrypted_len, src.get(block_src.data(), 16));
-		if(block_encrypted_len < 16){
-			// Remove PKCS#7 padding.
-			DEBUG_THROW_ASSERT(block_encrypted_len == 0);
-			DEBUG_THROW_ASSERT(!dst.empty());
-			const unsigned bytes_padded = static_cast<unsigned>(dst.back());
-			DEBUG_THROW_ASSERT((1 <= bytes_padded) && (bytes_padded <= 16));
-			for(unsigned i = 0; i < bytes_padded; ++i){
-				dst.unput();
-			}
-			break;
-		}
-		::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
-		dst.put(block_dst.data(), 16); // *: block encrypted
+	const std::size_t n_blocks_m1 = src.size() / 16 - 1;
+	if(n_blocks_m1 == (std::size_t)-1){
+		LOG_MEDUSA_WARNING("Encrypted data is invalid, final block not found.");
+		return false;
 	}
+	for(std::size_t i = 0; i < n_blocks_m1; ++i){
+		src.get(block_src.data(), 16);
+		::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
+		dst.put(block_dst.data(), 16);
+	}
+	// Remove PKCS#7 padding.
+	src.get(block_src.data(), 16);
+	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
+	const unsigned bytes_padded = block_dst.back();
+	LOG_MEDUSA_DEBUG("Removing ", bytes_padded, " padding byte(s).");
+	dst.put(block_dst.data(), 16 - bytes_padded);
 	return true;
 }
 
