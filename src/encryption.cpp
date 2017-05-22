7bacca7e8bec0fb9a2c0f5f662c3721de7eb2c2e
lhmouse <lh_mouse@126.com>
2017-05-23 00:31:33 +0800

    .

--- src/encryption.cpp
+++ src/encryption.cpp
@@ -6,16 +6,18 @@
 
 namespace Medusa {
 
+typedef boost::array<unsigned char, 16> AesBlock;
+
 void encrypt(Poseidon::StreamBuffer &dst, const Poseidon::Uuid &uuid, Poseidon::StreamBuffer src, const std::string &key){
 	PROFILE_ME;
 
-	dst.put(uuid.data(), uuid.size()); // 16 bytes: UUID
+	dst.put(uuid.data(), 16); // 16 bytes: UUID
 	boost::uint64_t nonce;
 	nonce = Poseidon::random_uint64();
-	dst.put(&nonce, sizeof(nonce)); // 8 bytes: nonce
+	dst.put(&nonce, 8); // 8 bytes: nonce
 	Poseidon::Sha256_ostream sha256_os;
-	sha256_os.write(reinterpret_cast<const char *>(uuid.data()), static_cast<std::streamsize>(uuid.size()))
-	         .write(reinterpret_cast<const char *>(&nonce), static_cast<std::streamsize>(sizeof(nonce)))
+	sha256_os.write(reinterpret_cast<const char *>(uuid.data()), 16)
+	         .write(reinterpret_cast<const char *>(&nonce), 8)
 	         .write(reinterpret_cast<const char *>(key.data()), static_cast<std::streamsize>(key.size()));
 	const AUTO(sha256, sha256_os.finalize());
 	::AES_KEY aes_key[1];
@@ -23,82 +25,63 @@ void encrypt(Poseidon::StreamBuffer &dst, const Poseidon::Uuid &uuid, Poseidon::
 		LOG_MEDUSA_FATAL("::AES_set_encrypt_key() failed!");
 		std::abort();
 	}
-	boost::array<unsigned char, 16> iv, block_dst, block_src;
+	AesBlock iv, in, out;
+	int num = 0;
 	std::fill(iv.begin(), iv.end(), 42);
-	std::copy(sha256.begin() + 16, sha256.end(), block_src.begin());
-	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
-	dst.put(block_dst.data(), 16); // 16 bytes: checksum
-	// Encrypt payload.
-	const std::size_t n_blocks_m1 = src.size() / 16;
-	for(std::size_t i = 0; i < n_blocks_m1; ++i){
-		DEBUG_THROW_ASSERT(src.get(block_src.data(), 16) == 16);
-		::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
-		dst.put(block_dst.data(), 16);
+	std::copy(sha256.begin() + 16, sha256.end(), in.begin());
+	::AES_cfb128_encrypt(in.data(), out.data(), 16, aes_key, iv.data(), &num, AES_ENCRYPT);
+	dst.put(out.data(), 16); // 16 bytes: checksum
+	for(;;){
+		const AUTO(bytes_read, src.get(in.data(), 16));
+		if(bytes_read == 0){
+			break;
+		}
+		::AES_cfb128_encrypt(in.data(), out.data(), bytes_read, aes_key, iv.data(), &num, AES_ENCRYPT);
+		dst.put(out.data(), bytes_read);
 	}
-	// Append PKCS#7 padding.
-	const unsigned bytes_padded = 16 - src.get(block_src.data(), 16);
-	LOG_MEDUSA_DEBUG("Appending ", bytes_padded, " padding byte(s).");
-	std::fill(block_src.end() - bytes_padded, block_src.end(), bytes_padded);
-	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_ENCRYPT);
-	dst.put(block_dst.data(), 16);
 }
 bool decrypt(Poseidon::Uuid &uuid, Poseidon::StreamBuffer &dst, Poseidon::StreamBuffer src, const std::string &key){
 	PROFILE_ME;
 
-	if(src.get(uuid.data(), uuid.size()) < 16){ // 16 bytes: UUID
+	if(src.get(uuid.data(), 16) < 16){ // 16 bytes: UUID
 		LOG_MEDUSA_WARNING("Encrypted data is truncated, expecting UUID.");
 		return false;
 	}
 	boost::uint64_t nonce;
-	if(src.get(&nonce, sizeof(nonce)) < 8){ // 8 bytes: nonce
+	if(src.get(&nonce, 8) < 8){ // 8 bytes: nonce
 		LOG_MEDUSA_WARNING("Encrypted data is truncated, expecting nonce.");
 		return false;
 	}
 	Poseidon::Sha256_ostream sha256_os;
-	sha256_os.write(reinterpret_cast<const char *>(uuid.data()), static_cast<std::streamsize>(uuid.size()))
-	         .write(reinterpret_cast<const char *>(&nonce), static_cast<std::streamsize>(sizeof(nonce)))
+	sha256_os.write(reinterpret_cast<const char *>(uuid.data()), 16)
+	         .write(reinterpret_cast<const char *>(&nonce), 8)
 	         .write(reinterpret_cast<const char *>(key.data()), static_cast<std::streamsize>(key.size()));
 	const AUTO(sha256, sha256_os.finalize());
-	boost::array<unsigned char, 16> checksum;
-	if(src.get(checksum.data(), checksum.size()) < 16){ // 16 bytes: checksum
+	::AES_KEY aes_key[1];
+	if(::AES_set_encrypt_key(sha256.data(), 128, aes_key) != 0){ // CFB 仅使用加密密钥。
+		LOG_MEDUSA_FATAL("::AES_set_encrypt_key() failed!");
+		std::abort();
+	}
+	AesBlock iv, in, out;
+	int num = 0;
+	std::fill(iv.begin(), iv.end(), 42);
+	if(src.get(in.data(), 16) < 16){ // 16 bytes: checksum
 		LOG_MEDUSA_WARNING("Encrypted data is truncated, expecting checksum.");
 		return false;
 	}
-	::AES_KEY aes_key[1];
-	if(::AES_set_decrypt_key(sha256.data(), 128, aes_key) != 0){
-		LOG_MEDUSA_FATAL("::AES_set_decrypt_key() failed!");
-		std::abort();
-	}
-	boost::array<unsigned char, 16> iv, block_dst, block_src;
-	std::fill(iv.begin(), iv.end(), 42);
-	std::copy(checksum.begin(), checksum.end(), block_src.begin());
-	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
-	if(!std::equal(block_dst.begin(), block_dst.end(), sha256.begin() + 16)){
+	::AES_cfb128_encrypt(in.data(), out.data(), 16, aes_key, iv.data(), &num, AES_DECRYPT);
+	if(!std::equal(out.begin(), out.end(), sha256.begin() + 16)){
 		LOG_MEDUSA_WARNING("Encrypted data is invalid, erroneous checksum.");
 		return false;
 	}
-	// Decrypt payload.
-	if(src.size() % 16 != 0){
-		LOG_MEDUSA_WARNING("Encrypted data is invalid, invalid size.");
-		return false;
+	for(;;){
+		const AUTO(bytes_read, src.get(in.data(), 16));
+		if(bytes_read == 0){
+			break;
+		}
+		::AES_cfb128_encrypt(in.data(), out.data(), bytes_read, aes_key, iv.data(), &num, AES_DECRYPT);
+		dst.put(out.data(), bytes_read);
 	}
-	const std::size_t n_blocks_m1 = src.size() / 16 - 1;
-	if(n_blocks_m1 == (std::size_t)-1){
-		LOG_MEDUSA_WARNING("Encrypted data is invalid, final block not found.");
-		return false;
-	}
-	for(std::size_t i = 0; i < n_blocks_m1; ++i){
-		DEBUG_THROW_ASSERT(src.get(block_src.data(), 16) == 16);
-		::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
-		dst.put(block_dst.data(), 16);
-	}
-	// Remove PKCS#7 padding.
-	DEBUG_THROW_ASSERT(src.get(block_src.data(), 16) == 16);
-	::AES_cbc_encrypt(block_src.data(), block_dst.data(), 16, aes_key, iv.data(), AES_DECRYPT);
-	const unsigned bytes_padded = block_dst.back();
-	LOG_MEDUSA_DEBUG("Removing ", bytes_padded, " padding byte(s).");
-	DEBUG_THROW_ASSERT((1 <= bytes_padded) && (bytes_padded <= 16));
-	dst.put(block_dst.data(), 16 - bytes_padded);
 	return true;
 }
 
