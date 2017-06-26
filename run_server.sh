7aaea9f61ee6056fda99170c5a9864213482212d
lhmouse <lh_mouse@126.com>
2017-06-26 16:42:49 +0800

    Update poseidon and add -vgdb for runserver.sh.

--- run_server.sh
+++ run_server.sh
@@ -7,6 +7,8 @@ if [ "$1" == "-d" ]; then
 	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute gdb --args poseidon $etc/poseidon-medusa
 elif [ "$1" == "-v" ]; then
 	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' poseidon $etc/poseidon-medusa
+elif [ "$1" == "-vgdb" ]; then
+	LD_LIBRARY_PATH=$runpath ./libtool --mode=execute valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --log-file='valgrind.log' poseidon $etc/poseidon-medusa
 else
 	LD_LIBRARY_PATH=$runpath poseidon $etc/poseidon-medusa
 fi
